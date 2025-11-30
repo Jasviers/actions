462d7559ad70665ee1ac20718c5e885de1e64206
jasviers <jasviers@gmail.com>
2025-11-30 23:44:51 +0100

    ci: modify doc conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath("../../"))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +19,12 @@ release = "1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -24,5 +34,5 @@ language = "es"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
